e583f9ed7797f14a758af9680328e85a27f73874
Andersson Velasquez Alvites <cxasper23@gmail.com>
2020-07-27 22:02:26 -0500

    ADD: initial models

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from colorfield.fields import ColorField
 
 
 # Create your models here.
@@ -15,7 +16,32 @@ class DateTable(models.Model):
 class MetaDataTable(DateTable):
     name = models.CharField(max_length=100, null=False)
     display_name = models.CharField(max_length=100, null=True)
-    description = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
 
     class Meta:
         abstract = True
+
+
+class Currency(MetaDataTable):
+    country = models.CharField(max_length=50, null=True)
+    code = models.CharField(max_length=4, null=False)
+
+
+class Account(MetaDataTable):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, models.CASCADE, null=False)
+    currency = models.ForeignKey(
+        Currency, models.DO_NOTHING, null=False)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+
+
+class TypeTransaction(MetaDataTable):
+    theme_color = ColorField(default='#FF0000')
+
+
+class Transaction(MetaDataTable):
+    account = models.ForeignKey(
+        Account, models.CASCADE, null=False)
+    type = models.ForeignKey(
+        TypeTransaction, models.DO_NOTHING, null=False)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
