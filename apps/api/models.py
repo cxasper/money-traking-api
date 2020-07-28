7fc11a706d9d4814416038e526330b96517f88e4
Andersson Velasquez Alvites <cxasper23@gmail.com>
2020-07-27 19:39:28 -0500

    ADD: basic abstract models

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class DateTable(models.Model):
@@ -8,3 +10,12 @@ class DateTable(models.Model):
     class Meta:
         abstract = True
         ordering = ['-created_at']
+
+
+class MetaDataTable(DateTable):
+    name = models.CharField(max_length=100, null=False)
+    display_name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=200, null=True)
+
+    class Meta:
+        abstract = True
