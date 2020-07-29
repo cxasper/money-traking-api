c514e90953ccb9c6010ef557e012d81a0f4fc63f
Andersson Velasquez Alvites <cxasper23@gmail.com>
2020-07-28 23:33:10 -0500

    ADD: add list and create accounts

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from apps.api.models import Account
+from apps.api.serializers import AccountSerializer
 
 # Create your views here.
+class AccountListCreate(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        self.queryset = Account.objects.filter(user=self.request.user)
+        return super().get_queryset()
+
+    def create(self, request, *args, **kwargs):
+        request.data.update({'user': request.user.id})
+        return super().create(request, *args, **kwargs)
